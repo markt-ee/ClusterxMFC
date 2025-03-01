7711b9a222a4b6d783c8654f5cf570a8d4e5b9a6
mkatherine <55812935+mkatherine@users.noreply.github.com>
2025-02-28 19:07:57 -0800

    files for saving data to mobile phones

---
+++ SaveData-Server.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/download')
+def download_file():
+    file_path = "/Users/katherinetedjokusumo/XCode/ClusterxMFC_Playground/SaveData/SaveData-TestFile.txt"  # Change this to your actual file
+    return send_file(file_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001)
+
+#192.168.1.158 IP address of mycafe
+#192.168.0.156 IP address of TPlink
