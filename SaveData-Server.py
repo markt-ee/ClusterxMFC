25f6866ef9c853d436004e43f013c3db705234b2
mkatherine <55812935+mkatherine@users.noreply.github.com>
2025-02-28 19:18:57 -0800

    updated the savedata-server.py and was able to run on windows.

--- SaveData-Server.py
+++ SaveData-Server.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/download')
 def download_file():
-    file_path = "/Users/katherinetedjokusumo/XCode/ClusterxMFC_Playground/SaveData/SaveData-TestFile.txt"  # Change this to your actual file
+    file_path = "C:/Users/kathe/Desktop/ClusterxMFC/SaveData-TestFile.txt"  # Change this to your actual file
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host="192.168.1.158", port=5001)
+    app.run(host="0.0.0.0", port=5001) #do not change
 
 #192.168.1.158 IP address of mycafe
 #192.168.0.156 IP address of TPlink
